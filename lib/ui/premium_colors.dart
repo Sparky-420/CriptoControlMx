723848d7f38624b4448a8ba8de53fcf2bf5d0bc9
Daniel Vargas <danyvargas929@gmail.com>
2026-06-17 21:37:30 -0600

    Extract premium color tokens

---
+++ lib/ui/premium_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PremiumColors {
+  static const Color bootBackground = Color.fromARGB(255, 7, 17, 22);
+  static const Color bootBackgroundAlt = Color.fromARGB(255, 11, 29, 32);
+  static const Color bootSurface = Color.fromARGB(255, 16, 20, 28);
+  static const Color primary = Color.fromARGB(255, 54, 211, 153);
+  static const Color mutedText = Color.fromARGB(255, 184, 199, 193);
+}
