dd3910ec1f92d20bc3f62b79823fb775a8a9619f
Daniel Vargas <danyvargas929@gmail.com>
2026-09-10 19:17:57 -0600

    fix: raise Kotlin to Flutter minimum

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     id("com.google.gms.google-services") version("4.4.1") apply false
     id("com.google.firebase.crashlytics") version("3.0.7") apply false
     // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
